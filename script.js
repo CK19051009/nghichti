0a474d090f0458b99ffee8e4279469d5fe2cd37d
Your Name <you@example.com>
2024-07-25 23:08:56 +0700

    day roi

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+const button = document.querySelector(".inner__button");
+const title = document.querySelector(".inner__title");
+
+button.addEventListener("click" , () =>{
+        title.classList.toggle("show");
+
+})  
